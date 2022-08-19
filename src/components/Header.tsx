3e120aac693b1cd6d44eec6ce166ff2930441e13
fkrein1 <felipekrein@gmail.com>
2022-08-19 10:23:25 -0300

    feat change logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import logo from '../assets/logo-quiz.png'
 
 export function Header() {
   return (
-    <div className="text-3xl pb-2 pt-3 text-center border-b-2 flex justify-center">
-      <img src={logo} className="h-24"/>
+    <div className="text-3xl p-1 text-center border-b-2 flex justify-center">
+      <img src={logo} className="h-16"/>
     </div>
   );
 }
